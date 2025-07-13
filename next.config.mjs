f2ac572482df3b8276d33f9c620e305ba2d7c6d9
bheema-284 <bheemudug@gmail.com>
2025-07-13 13:38:22 +0530

    refacto: add font

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['var(--font-sans)', 'sans-serif'],
+                mono: ['var(--font-mono)', 'monospace'],
+            },
+        },
+    },
+};
 
 export default nextConfig;
