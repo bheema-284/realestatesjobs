e53db0762d0b9fb51a6f400f2f92c27080bcaed3
bheema-284 <bheemudug@gmail.com>
2025-07-22 18:46:12 +0530

    refactor: change text in home page

--- app/page.js
+++ app/page.js
@@ -1,6 +1,10 @@
 
 export default function Home() {
   return (
-    <div className="text-[20px] text-red-500 flex justify-center items-center h-[80vh]">Home Page</div>
+    <div className="text-[20px] text-red-500 flex justify-center items-center h-[80vh]">
+      <h1 className="text-3xl sm:text-4xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-yellow-400 via-pink-500 to-purple-700">
+        This is Real Estate Jobs Home Page
+      </h1>
+    </div>
   );
 }
