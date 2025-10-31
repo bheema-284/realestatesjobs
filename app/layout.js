51966118ae190f1498ae6571ba7d23dff0dea046
bheema-284 <bheemudug@gmail.com>
2025-10-31 17:49:06 +0530

    feat: add fav icon

--- app/layout.js
+++ app/layout.js
@@ -9,6 +9,7 @@ import { Inter } from 'next/font/google';
 import Footer from "@/components/common/footer";
 import Sidebar from "@/components/common/sidebar";
 import { Navbar } from "@/components/common/navbar";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"], display: "swap" });
 
@@ -824,6 +825,10 @@ export default function RootLayout({ children }) {
 
   return (
     <html lang="en" className={inter.variable}>
+      <Head>
+        {/* ✅ Add favicon here */}
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+      </Head>
       <body className="w-full mx-auto">
         <RootContext.Provider value={{ rootContext, setRootContext }}>
           {(pathName !== "/login" && pathName !== "/signup") && (
