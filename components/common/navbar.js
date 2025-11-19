49f96a607036888fde7bef63802b8e38b5b01666
bheema-284 <bheemudug@gmail.com>
2025-11-19 13:46:36 +0530

    refactor: handle notification

--- components/common/navbar.js
+++ components/common/navbar.js
@@ -248,7 +248,7 @@ export const Navbar = ({ rootContext, showLoader, logOut }) => {
                 >
                     <BellIcon className="w-5 h-5" />
                     {unreadCount > 0 && (
-                        <span className="absolute -top-0.5 -right-4 bg-red-500 text-white text-[11px] rounded-full w-4 h-4 flex items-center justify-center">
+                        <span className="absolute -top-0.5 -right-1 bg-red-500 text-white text-[11px] rounded-full w-4 h-4 flex items-center justify-center">
                             {unreadCount > 9 ? '9+' : unreadCount}
                         </span>
                     )}
