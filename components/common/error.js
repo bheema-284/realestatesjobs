6abb0cfad6f42fc46aa410dcb3e0ae2ad831d591
bheema-284 <bheemudug@gmail.com>
2026-02-05 15:26:31 +0530

    feat: add error page

---
+++ components/common/error.js
@@ -0,0 +1,7 @@
+export default function Error() {
+    return (
+        <div className="rounded-md bg-red-50 p-4 mt-10 flex mx-3">
+            <h3 className="text-sm font-medium text-red-800">Error</h3>
+        </div>
+    )
+}
